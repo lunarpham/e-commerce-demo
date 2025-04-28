6b76835f5e4bb3a3a9b883249c95998b47db2810
lunarpham <truong.pham210214@vnuk.edu.vn>
2025-04-28 14:59:35 +0700

    added util function to return formatted date with time

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -3,3 +3,15 @@ export function formatDate(dateString) {
   const date = new Date(dateString);
   return date.toLocaleDateString("en-US", options);
 }
+
+export function formatDateWithTime(dateString) {
+  const options = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  };
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-US", options);
+}
